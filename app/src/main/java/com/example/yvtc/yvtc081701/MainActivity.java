1b4356cb5351ba0bec607399854bf9a67186484b
Shaojung <shaojung@gmail.com>
2017-08-17 09:06:04 +0800

    use onProgressUpdate

--- app/src/main/java/com/example/yvtc/yvtc081701/MainActivity.java
+++ app/src/main/java/com/example/yvtc/yvtc081701/MainActivity.java
@@ -5,40 +5,49 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tv, tv2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.textView);
+        tv2 = (TextView) findViewById(R.id.textView2);
         MyTask task = new MyTask();
         task.execute("5");
     }
-}
+    class MyTask extends AsyncTask<String, Integer, String>
+    {
 
-class MyTask extends AsyncTask<String, Integer, String>
-{
-
-    @Override
-    protected String doInBackground(String... params) {
-        int i = Integer.valueOf(params[0]);
-
-        for (int j = i; j>0;j--)
-        {
-            Log.d("MYTASK", "in doInBackground:" + String.valueOf(j));
-            try {
-                Thread.sleep(500);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            tv.setText(String.valueOf(values[0]));
         }
-        return "Okay";
-    }
 
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-        Log.d("MYTASK", "on postexecute:" + s);
+        @Override
+        protected String doInBackground(String... params) {
+            int i = Integer.valueOf(params[0]);
+
+            for (int j = i; j>0;j--)
+            {
+                publishProgress(j);
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return "Okay";
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            tv2.setText(s);
+        }
     }
 }
+
